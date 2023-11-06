77471b2408f0be45e84cb295c78c1b83f9d57e90
atulsy9 <atulsy9@gmail.com>
2023-11-07 03:15:33 +0530

    card-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <Card />
     </>
   );
 }
